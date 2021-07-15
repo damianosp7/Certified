83a89b6d6d5ef855065ea741fdf43f03197f5092
damianosp7 <da.67-@windowslive.com>
2021-07-15 21:10:08 +0300

    1 - First Commit

---
+++ src/main/java/com/example/certified/CertifiedApplication.java
@@ -0,0 +1,13 @@
+package com.example.certified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifiedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertifiedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/certified/CertifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifiedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
