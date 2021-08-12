43b5863412d05e49dbff10af20445b1fa51f98fb
damianosp7 <da.67-@windowslive.com>
2021-08-12 15:22:18 +0300

    6 - Add user management methods and UI(Update,Delete,Add) in both roles

--- src/main/java/com/example/certified/service/DocStorageService.java
+++ src/main/java/com/example/certified/service/DocStorageService.java
@@ -3,9 +3,13 @@ package com.example.certified.service;
 import com.example.certified.model.Docs;
 import com.example.certified.repository.DocRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +20,16 @@ public class DocStorageService {
 
     public Docs saveFile(MultipartFile file){
         String docname =  file.getOriginalFilename();
+        Object authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String username = " ";
+        if (authentication instanceof UserDetails){
+            username = ((UserDetails)authentication).getUsername();
+        }
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
         try {
-            Docs doc = new Docs(null,docname , file.getContentType(), null, file.getBytes());
+            Docs doc = new Docs(username,docname , file.getContentType(), timestamp.toString(), file.getBytes());
             return docRepository.save(doc);
         }
         catch (Exception e){
--- src/main/java/com/example/certified/web/MainController.java
+++ src/main/java/com/example/certified/web/MainController.java
@@ -28,5 +28,133 @@ public class MainController {
         return "login";
     }
 
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") @Valid User usertosave, BindingResult result){
+        if (result.hasErrors()) {
+            return "new_user";
+        }
+        //save user to database
+        return userService.saveUser(usertosave);
+//        return "redirect:/showNewUserForm?success";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute("user") @Valid User user, BindingResult result){
+        //check id the logged in user is the administrator
+        Object authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails)authentication).getUsername();
+
+        if (result.hasErrors() && username == "root") {
+            return "update_users";
+        }
+        else if(result.hasErrors()){
+            return "update_user";
+        }
+        //save user to database
+        return userService.updateUser(user);
+//        return "redirect:/";
+    }
+
+
+//    //display user
+//    @GetMapping("/user/{username}")
+//    public String ViewAccountPage(Model model, @PathVariable(value = "username") String username){
+//        model.addAttribute("listUser", userRepository.findByEmail(username));
+//        return "user";
+//    }
+
+
+    //display user  and take id from back-end
+    @GetMapping("/user")
+    public String ViewAccountPage(Model model){
+        Object authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails)authentication).getUsername();
+        model.addAttribute("listUser", userRepository.findByEmail(username));
+        return "user";
+    }
+
+    //display list of users
+    @GetMapping("/users")
+    public String ViewAllAccountsPage(Model model){
+        return findPaginated(1,"firstName","asc", model);
+    }
+
+    @GetMapping("/showNewUserForm")
+    public String showNewUserForm(Model model){
+        //create model attribute to bind form data
+        User user = new User();
+        model.addAttribute("user", user);
+        return "new_user";
+    }
+//    if you want to show id in front end during update user disable this method's comments (not safe)
+//    @GetMapping("/showFormForUpdate/{id}")
+//    public String showFormForUpdate(@PathVariable ( value = "id") Long id, Model model) {
+//        // get employee from the service
+//        User user = userService.getUserById(id);
+//        // set employee as a model attribute to pre-populate the form
+//        model.addAttribute("user", user);
+//        return "update_user";
+//    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdateUser( Model model) {
+        // get employee from the service
+        Object authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails)authentication).getUsername();
+        Long id = userRepository.findByEmail(username).getId();
+        User user = userService.getUserById(id);
+        // set employee as a model attribute to pre-populate the form
+        model.addAttribute("user", user);
+        return "update_user";
+    }
+
+    @GetMapping("/showFormForUpdateAdmin/{id}")
+    public String showFormForUpdateAdmin(@PathVariable ( value = "id") Long id, Model model) {
+        // get employee from the service
+        User user = userService.getUserById(id);
+        if(user.getEmail().equals("root")){
+            return "redirect:/users?admin";
+        }
+        else {
+            // set employee as a model attribute to pre-populate the form
+            model.addAttribute("user", user);
+            return "update_users";
+        }
+    }
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable (value = "id") long id){
+        //call delete user method
+        User user = userService.getUserById(id);
+        if(user.getEmail().equals("root")){
+            return "redirect:/users?admin";
+        }
+        else {
+            this.userService.deleteUserById(id);
+            return "redirect:/users";
+        }
+    }
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model){
+        int pageSize = 10 ;
+
+        Page<User> page = userService.findPaginate(pageNo,pageSize, sortField, sortDir);
+        List<User> listUsers = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc")? "desc": "asc");
+
+        model.addAttribute("listUsers", listUsers);
+        return "users";
+    }
+
 
 }
